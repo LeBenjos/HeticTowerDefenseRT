8d12cec2b184ac7e8b293bec0226baad1c157cf0
LeBenjos <benjamin.schinkel01@outlook.fr>
2025-04-17 16:57:58 +0200

    feat: enemy animation

--- Assets/Games/Scripts/Gameplay/Enemy.cs
+++ Assets/Games/Scripts/Gameplay/Enemy.cs
@@ -4,30 +4,58 @@ public class Enemy : MonoBehaviour
 {
     private Transform target;
     private EnemyPool pool;
-    [SerializeField] private float speed = 0.5f;
-    [SerializeField] private float dispawnDistance = 0.1f;
+    private Animator animator;
 
-    public void Start()
+    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private float despawnDistance = 0.1f;
+
+    private EnemyState currentState;
+
+    private void Awake()
     {
-        Debug.Log("Create enemy");
+        animator = GetComponent<Animator>();
     }
 
     public void Initialize(Transform target, EnemyPool pool)
     {
         this.target = target;
         this.pool = pool;
+
+        SetState(EnemyState.Spawning);
     }
 
-    void Update()
+    private void SetState(EnemyState newState)
     {
-        if (target == null) return;
+        currentState = newState;
+
+        switch (newState)
+        {
+            case EnemyState.Spawning:
+                animator.Play("EnemySpawn");
+                transform.LookAt(target.position);
+                break;
+
+            case EnemyState.Moving:
+                animator.Play("EnemyRun");
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        if (target == null || currentState != EnemyState.Moving) return;
 
         transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
         transform.LookAt(target.position);
 
-        if (Vector3.Distance(transform.position, target.position) < dispawnDistance)
+        if (Vector3.Distance(transform.position, target.position) < despawnDistance)
         {
             pool.ReturnEnemy(gameObject);
         }
+    }
+
+    public void OnSpawnAnimationFinished()
+    {
+        SetState(EnemyState.Moving);
     }
 }
