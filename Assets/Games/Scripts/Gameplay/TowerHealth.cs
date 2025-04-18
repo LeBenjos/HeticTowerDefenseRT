d4b12fb6ebba185ba36b1039008c9334cad3cf1d
Maria Petersen <maria.petersen@ealico.com>
2025-04-18 14:37:50 +0200

    ADD health bar

--- Assets/Games/Scripts/Gameplay/TowerHealth.cs
+++ Assets/Games/Scripts/Gameplay/TowerHealth.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TowerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
 
+	public Slider healthBar;
+
     void Start()
     {
         currentHealth = maxHealth;
+
+		if (healthBar != null)
+		{
+    		healthBar.maxValue = maxHealth;
+   		 	healthBar.value = currentHealth;
+		}
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+		UpdateHealthBar();
         Debug.Log("Tower took damage. Current health: " + currentHealth);
 
         if (currentHealth <= 0)
@@ -26,5 +36,13 @@ public class TowerHealth : MonoBehaviour
     {
         Debug.Log("Tower destroyed!");
        	GameManager.Instance.TriggerGameOver();
+    }
+
+	void UpdateHealthBar()
+    {
+        if (healthBar != null)
+        {
+            healthBar.value = currentHealth;
+        }
     }
 }
