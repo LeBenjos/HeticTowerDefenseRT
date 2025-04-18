d05d7953854532278af83a4f1909160c106de16a
Maria Petersen <maria.petersen@ealico.com>
2025-04-18 15:21:31 +0200

    improve game over message

--- Assets/Games/Scripts/Gameplay/TowerHealth.cs
+++ Assets/Games/Scripts/Gameplay/TowerHealth.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
 
 public class TowerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
-
 	public Slider healthBar;
+    private float targetHealth;
+    public float lerpSpeed = 5f;
 
     void Start()
     {
         currentHealth = maxHealth;
+        targetHealth = currentHealth;
 
 		if (healthBar != null)
 		{
@@ -19,10 +22,25 @@ public class TowerHealth : MonoBehaviour
    		 	healthBar.value = currentHealth;
 		}
     }
-
+    
+    void Update()
+    {
+        if (Mathf.Abs(targetHealth - healthBar.value) > 0.1f)
+        {
+            healthBar.value = Mathf.Lerp(healthBar.value, targetHealth, Time.deltaTime * lerpSpeed);
+        }
+        else
+        {
+            healthBar.value = targetHealth; 
+        }
+    }
+    
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        targetHealth = currentHealth;
+        
 		UpdateHealthBar();
         Debug.Log("Tower took damage. Current health: " + currentHealth);
 
